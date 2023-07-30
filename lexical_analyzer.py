f8961f2a9037408b7ea067d557b5a80492fdb494
Ahmed Faiyaz <69722755+afaiyaz006@users.noreply.github.com>
2023-07-31 01:57:49 +0600

    Update lexical_analyzer.py

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -6,18 +6,18 @@ key_words=[
         "auto",
         "break",
         "case",
-        "char", 
-        "const", 
+        "char",
+        "const",
         "continue",
         "default",
         "do",
         "double",
-        "else", 
-        "enum", 
+        "else",
+        "enum",
         "extern",
-        "float", 
-        "for", 
-        "goto", 
+        "float",
+        "for",
+        "goto",
         "if",
         "int",
         "long",
@@ -28,11 +28,11 @@ key_words=[
         "sizeof",
         "static",
         "struct",
-        "switch", 
-        "typedef", 
+        "switch",
+        "typedef",
         "union",
         "unsigned",
-        "void", 
+        "void",
         "volatile",
         "while"
 ]
@@ -42,40 +42,48 @@ def remove_dups(datas:list)->list:
     """
     return list(OrderedDict.fromkeys(datas))
 
-def lexical_analyzer(source_code:str)->dict[str:list[str]]:
+
+def lexical_analyzer(program:str)->dict[str:list[str]]:
     """
         A simple lexical analyzer function
         :input:
-            source_code:
-            Source code of the program.
+            program:
+            A program in c.
+
 
         :output:
-            Dictionary containing tokens of various types.
+            dictionary containing tokens of various types.
             "keywords":keywords,
             "identifiers":identifiers,
             "arithmatic_operators":arithmatic_operators,
             "constants":constants,
             "punctuations_signs":punctuations_signs,
+            "arithmatic_operators":arithmatic_operators,
             "logical_operators":logical_operators,
             "parenthesis":parenthesis,
-            
+           
     """
+
 
     # for removing any kind of comments
     comment_pattern=r"//.*\n"
-    source_code=re.split(comment_pattern,source_code)
-    source_code="".join(source_code)
+    program=re.split(comment_pattern,program)
+    program="".join(program)
+
+
 
 
     #for finding token types
     identifiers=re.findall("[a-zA-Z]+[0-9]|[a-zA-Z]+",program)
     constants=re.findall("[0-9]+",program)
-    punctuations_signs=re.findall("[;]|[-]|[,]",program)
+
+
+    punctuations_signs=re.findall("[;]|[:]|[,]",program)
     arithmatic_operators=re.findall("[+]|[-]|[*]|[=]|[/]",program)
-    logical_operators=re.findall("[>]|>=|<|<=|==|!=",program)
-    parenthesis=re.findall("[{]|[}]|[(]|[)]|[{]|[}]",program)
+    logical_operators=re.findall("<=|>=|[<]|[>]|==|!=",program)
+    parentheses=re.findall("[{]|[}]|[(]|[)]|[[]|[]]",program)
     keywords=re.findall("|".join(key_words),program)
-    
+   
     #removing duplicates
     identifiers=remove_dups(identifiers)
     punctuations_signs=remove_dups(punctuations_signs)
@@ -83,12 +91,14 @@ def lexical_analyzer(source_code:str)->dict[str:list[str]]:
     logical_operators=remove_dups(logical_operators)
     keywords=remove_dups(keywords)
 
+
     #removing keyword which were identified as identifier
     #regex likhar iccha chilo but parlam na
     for key_word in keywords:
         if key_word in identifiers:
             identifiers.remove(key_word)
-    
+   
+
 
     return {
         "keywords":keywords,
@@ -96,28 +106,30 @@ def lexical_analyzer(source_code:str)->dict[str:list[str]]:
         "arithmatic_operators":arithmatic_operators,
         "constants":constants,
         "punctuations_signs":punctuations_signs,
+        "arithmatic_operators":arithmatic_operators,
         "logical_operators":logical_operators,
-        "parenthesis":parenthesis,
+        "parenthesis":parentheses,
     }
-    
+   
 if __name__=='__main__':
-    source_code=""""""
+    program=""""""
     while True:
         try:
             line=input()
             if line=="EOF":
                 break
-            source_code+=line+"\n"
+            program+=line+"\n"
         except EOFError:
             break    
 
-    lexemes=lexical_analyzer(source_code)
+
+    lexemes=lexical_analyzer(program)
+
 
     print(f"Keywords({len(lexemes['keywords'])}): {' , '.join(lexemes['keywords'])}")
     print(f"Identifiers({len(lexemes['identifiers'])}): {' , '.join(lexemes['identifiers'])}")
     print(f"Constants({len(lexemes['constants'])}): {' , '.join(lexemes['constants'])}")
     print(f"Punctuations signs({len(lexemes['punctuations_signs'])}): {' , '.join(lexemes['punctuations_signs'])}")
-    print(f"Arithmatic operators({len(lexemes['arithmatic_operators'])}): {' , '.join(lexemes['arithmatic_operators'])}")
+    print(f"Arithmetic operators({len(lexemes['arithmatic_operators'])}): {' , '.join(lexemes['arithmatic_operators'])}")
     print(f"Logical operators({len(lexemes['logical_operators'])}): {' , '.join(lexemes['logical_operators'])}")
     print(f"Parenthesis({len(lexemes['parenthesis'])}): {' , '.join(lexemes['parenthesis'])}")
-    
