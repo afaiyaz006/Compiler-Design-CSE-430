4469947335e7f63aa5a817d593a982761e188559
Ahmed Faiyaz <69722755+afaiyaz006@users.noreply.github.com>
2023-10-02 22:17:07 +0600

    added poorly implemented first follow algorithm. :disappointed_relieved:

--- first_follow.py
+++ first_follow.py
@@ -1,42 +1,84 @@
 from left_recursion_elimination_algorithm import remove_left_recursion
 
 
-def find_first(grammer:dict[str:str]):
+def find_first(grammer:dict[str:list[str]]):
+    first_Y={}
     nonterminals=list(grammer.keys())
-    reversed(nonterminals)
-    firsts=set()
-    firsts_dict={k:None for k in nonterminals}
-    def first_(key):
+    nonterminals.reverse()
+
+    for nonterminal,productions in grammer.items():
+        for production in productions:
+            for symbol in production:
+                if symbol not in nonterminals:
+                    first_Y[symbol]=set(symbol)
+        if '∈' in productions:
+            first_Y[nonterminal]=set('∈')
+        else:
+            first_Y[nonterminal]=set()
         
-        for production in grammer[key]:
-            pointer=0
-            while(pointer<len(grammer[key])):
-                first_char=production[pointer][0]
-                if first_char in nonterminals:
-                    if '∈' in grammer[first_char]:
-                        pointer+=1
+    del(first_Y['∈'])
+   
+    for i in range(0,len(nonterminals)**2):
+        epsilon_counter=0
+        for nonterminal,productions in grammer.items():
+            for production in productions:
+                for symbol in production:
+                   
+                    if symbol in nonterminals and '∈' in first_Y[symbol]:
+                            epsilon_counter+=1
+                            first_Y[nonterminal]=first_Y[nonterminal].union(first_Y[symbol])
+                    else:
+                        if symbol!='∈':
+                            first_Y[nonterminal]=first_Y[nonterminal].union(first_Y[symbol])
+                            break
+        #if epsilon_counter==0:
+        #    break
 
+    return first_Y,nonterminals
+
+
+def find_follow(grammer:dict[str:list[str]],first_Y:dict[str:set],nonterminals:list[str],start_symbol:str):
+
+    follow_Y={nonterminal:set() for nonterminal in nonterminals}
+    follow_Y[start_symbol].add('$')
+    
+    for steps in range(0,len(nonterminals)**2):
+        for nonterminal,productions in grammer.items():
+            for production in productions:
+                
+                for i in range(0,len(production)):
                     
-
-            else:
-                firsts.add(production[0])
-        return firsts
-    
+                    if production[i] in nonterminals:
+                        Beta=production[i]
+                        
+                        if i==len(production)-1:
+                            follow_Y[Beta]=follow_Y[nonterminal]
+                        else:
+                            for j in range(i+1,len(production)):
+                                if '∈' in first_Y[production[j]]:
+                                    follow_Y[Beta]=follow_Y[Beta].union(first_Y[production[j]]).union(follow_Y[nonterminal])
+                                else:
+                                    follow_Y[Beta]=follow_Y[Beta].union(first_Y[production[j]])
+                                    break
     for nonterminal in nonterminals:
-        #print(f"{nonterminal}: {first_(nonterminal)}")
-        firsts_dict[nonterminal]=first_(nonterminal)
-        firsts=set()
-    return firsts_dict
-
-
-
+       if '∈' in  follow_Y[nonterminal]:
+           follow_Y[nonterminal].remove('∈')
     
+    return follow_Y
 
 
+def print_grammer(grammer:dict[str:list[str]]):
+    for nonterminal,production in grammer.items():
+        print(f"{nonterminal}-> {'|'.join(production)}")
 
 
 if __name__=='__main__':
     grammers="""
+###
+E->XTa,
+X->+E|∈,
+T->iY|(E),
+Y->*T|∈
 ###
 S->aBDh,
 B->cC,
@@ -45,12 +87,37 @@ D->EF,
 E->g|∈,
 F->f|∈
 ###
+E->TR,
+R->+TR|∈,
+T->FY,
+Y->*FY|∈,
+F->(E)|i
+###
 """
     grammers=grammers.replace('\n','')
     grammers=grammers.split("###")
-   
+
     for grammer in grammers:
         if grammer!='':
+            
+            
+            start_symbol=grammer[0]
             grammer=remove_left_recursion(grammer)
-            print(grammer)
-            print(find_first(grammer))
+            if grammer is not None:
+                print("-----------------------------")
+                print("Input Grammer: ")
+                print_grammer(grammer)
+                
+                firsts_Y,nonterminals=find_first(grammer)
+                nonterminals.reverse()
+                
+                follow_Y=find_follow(grammer,firsts_Y,nonterminals,start_symbol)
+                print("------------------------------")
+                for symbol,firsts in firsts_Y.items():
+                    if symbol in nonterminals:
+                        print(f"First({symbol})-> {firsts_Y[symbol]}")
+                print("")
+                for symbol,firsts in follow_Y.items():
+                    if symbol in nonterminals:
+                        print(f"Follow({symbol})-> {follow_Y[symbol]}")
+                print("-------------------------------")          
